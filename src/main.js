578c7204e2c333449206a8a529d6e650a927c41f
ThePyroPenguin0 <151252058+ThePyroPenguin0@users.noreply.github.com>
2025-02-11 01:23:14 -0800

    Updated readme copy in main.js.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@
  * I believe them to be vapid, shallow, and not actually games that are games in the formal sense. No strategy
  * or conscious change in behavior can really affect the outcome - not least because there is literally no win state.
  * To that end, this "game" is really a fully functional endless runner - just one in which the player's task is to
- * keep the computer playing it for as long as it and the player can stand.
+ * keep the computer playing it for as long as it and the player can stand. (Due to the unusual nature of this game,
+ * I consulted with Professor Altice about this to get his approval. I believe he called it "conceptual art.")
  * 
  * UNDER NO CIRCUMSTANCES PLAY THIS GAME IF YOU ARE EPILEPTIC OR PRONE TO SEIZURES.
  * 
